bc4876ff780d9a17f0fa084038c5deb0307cb734
Cagdas Yonder <cagdasyonder@gmail.com>
2019-12-29 22:46:18 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import data from './data.json'
+import Chart from './Chart'
 
 function App() {
+  const config = {
+    data: data,
+    options: {
+      duration: 1000,
+      numberOfBars: 10,
+      showLegend: true
+    },
+    colors: [
+      '#a6cee3',
+      '#1f78b4',
+      '#b2df8a',
+      '#33a02c',
+      '#fb9a99',
+      '#e31a1c',
+      '#fdbf6f',
+      '#ff7f00',
+      '#cab2d6',
+      '#6a3d9a'
+    ]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Chart config={config} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
